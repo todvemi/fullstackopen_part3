f6830dcea912d45fe710c6eeaccc28c99ad5c737
Tomi Nikku <tomi.jmn@gmail.com>
2023-11-20 22:59:46 +0200

    exc 3.6 done

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 let persons = [
     {id: 1, name: "Arto Hellas", number: "040-123456"},
     {id: 2, name: "Ada Lovelace", number: "39-44-5323523"},
@@ -35,10 +37,40 @@ app.get('/api/persons/:id', (req, res) => {
 app.delete('/api/persons/:id', (req, res) => {
     const id = Number(req.params.id)
     persons = persons.filter(person => person.id !== id)
-
     res.status(204).end()
 })
 
+const generateId = () => {
+    return Math.floor(Math.random() * 9999)
+}
+
+app.post('/api/persons', (req, res) => {
+    const names = persons.map(person => person.name.toLowerCase())
+    const body = req.body
+    
+    if (!body.name) {
+        return res.status(400).json({
+            error: 'name missing'
+        })
+    } else if (!body.number) {
+        return res.status(400).json({
+            error: 'number missing'
+        })
+    } else if (names.includes(body.name.toLowerCase())) {
+        return res.status(400).json({
+            error: 'name must be unique'
+        })
+    }
+
+    const person = {
+        id: generateId(),
+        name: body.name,
+        number: body.number
+    }
+    
+    persons = persons.concat(person)
+    res.json(person)
+})
 
 const PORT = 3001
 app.listen(PORT, () => {
